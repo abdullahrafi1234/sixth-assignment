c2fb03f1530c7329f76472e9f67fa1633a9a32a0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-03-04 19:11:50 +0600

    added banner and disscuss section

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const discussSection = async () => {
+//     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+//     const data = await res.json()
+//     const cards = data.posts
+//     // console.log(cards)
+//     displayCards(cards)
+// }
+
+// const displayCards = posts => {
+//     const cardContainer = document.getElementById('card-container')
+
+//     posts.forEach(post=>{
+//         console.log(post)
+//         const postDiv = document.createElement('div')
+
+//         console.log(postDiv)
+//     })
+
+// }
+
+
+
+
+// discussSection()
