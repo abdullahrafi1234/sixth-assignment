382c80fd31133e32a3097c8c3bf381f9adeb717b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-03-04 21:50:01 +0600

    added fooer

---
+++ js/latest.js
@@ -0,0 +1,45 @@
+const latestSection = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const data = await res.json()
+    const cards = data
+    // console.log(cards)
+    displayLatestCard(cards)
+}
+
+const displayLatestCard = posts => {
+    const latestContainer = document.getElementById('latest-container')
+
+    posts.forEach(post => {
+        console.log(post)
+        const latestDiv = document.createElement('div')
+        latestDiv.classList = `card bg-base-100 shadow-xl p-8`
+        latestDiv.innerHTML = `
+        <figure><img src="${post.cover_image}" alt="Shoes" /></figure>
+
+        <div class="card-body text-[#12132D99]">
+        <h2 class="flex gap-2"> 
+            <span><img src="images/calender.png" alt=""></span>
+            ${post.author.posted_date
+            }</h2>
+
+        <p class="text-[#12132D] text-[18px] font-extrabold">${post.title}</p>
+
+        <p class="text-[#12132D99]">${post.description}</p>
+
+        <h2 class="flex gap-3 items-center"> <span><img class="w-[50px] rounded-full" src="${post.profile_image}" alt=""></span>
+
+            <div>
+                <p class="font-bold text-[#12132D] ">${post.author.name}</p>
+                <p class="text-[#12132D99]">${post.author.designation
+                }</p>
+            </div>
+        </h2>
+        
+        </div>
+        
+        `;
+        latestContainer.appendChild(latestDiv)
+    })
+}
+
+latestSection()
--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
-// const discussSection = async () => {
-//     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
-//     const data = await res.json()
-//     const cards = data.posts
-//     // console.log(cards)
-//     displayCards(cards)
-// }
+const discussSection = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+    const data = await res.json()
+    const cards = data.posts
+    // console.log(cards)
+    displayCards(cards)
+}
 
-// const displayCards = posts => {
-//     const cardContainer = document.getElementById('card-container')
+const displayCards = posts => {
+    const cardContainer = document.getElementById('card-container')
 
-//     posts.forEach(post=>{
-//         console.log(post)
-//         const postDiv = document.createElement('div')
+    posts.forEach(post=>{
+        // console.log(post)
+        const postDiv = document.createElement('div')
+        postDiv.classList= `card flex flex-col lg:flex-row card-side bg-base-100 shadow-xl p-4 border border-gray-300 mt-4 mb-4`
 
-//         console.log(postDiv)
-//     })
+        postDiv.innerHTML = `
+        <figure class=""><img class=" w-[200px] p-8" src="${post.image}"
+        alt="Movie" /></figure>
+        <div class="card-body">
 
-// }
+            <div class="flex flex-col lg:flex-row space-y-3">
+                <p class="text-lg font-medium "># ${post.category}</p>
+                <p class="text-lg font-medium ">
+                    Author: ${post.author.name}
+            </div>
+
+            <h2 class="card-title font-bold">${post.title}</h2>
+            <p class="text-[#131318]">${post.description}</p>
+            <br>
+
+            <hr>
+
+            <div class="flex mt-4">
+                <p class="flex gap-2">
+                    <img src="Images/comment.png" alt="">
+                    <span class="font-medium text-[#13131880]">${post.comment_count}</span>
+                </p>
+                <p class="flex gap-2">
+                    <img src="Images/eye.png" alt="">
+                    <span class="font-medium text-[#13131880]">${post.view_count}</span>
+                </p>
+                <p class="flex gap-2">
+                    <img src="Images/time.png" alt="">
+                    <span class="font-medium text-[#13131880]">${post.posted_time
+                    } min</span>
+                </p>
+                <p class="flex gap-2">
+                    <img src="Images/email 1.png" alt="">
+                                    
+                </p>
+            </div>
+        </div>
+        
+        `;
+
+        cardContainer.appendChild(postDiv)
+
+        // console.log(postDiv)
+    })
+
+}
 
 
 
 
-// discussSection()
+discussSection()
